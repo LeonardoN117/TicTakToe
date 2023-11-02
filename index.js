85033aa7348ea0dcc97bbd9f9e06cb0c3fa798a5
RobstaLobsta1 <rloeffler@csu.fullerton.edu>
2023-11-02 14:15:27 -0700

    Web Storage Function

--- index.js
+++ index.js
@@ -20,27 +20,42 @@ initializeGame();
 function initializeGame(){
     cells.forEach(cell => cell.addEventListener("click", cellClicked));
     restartBtn.addEventListener("click", restartGame);
-    statusText.textContent = `${currentPlayer}'s turn`;
-    running = true;
+
+    // Load the game state from local storage
+    loadGameState();
+
+    // Check if the game was running when the browser was closed
+    if (running) {
+        statusText.textContent = `${currentPlayer}'s turn`;
+    } else {
+        statusText.textContent = "Game Over";
+    }
 }
+
 function cellClicked(){
     const cellIndex = this.getAttribute("cellIndex");
 
-    if(options[cellIndex] != "" || !running){
+    if (options[cellIndex] != "" || !running) {
         return;
     }
 
     updateCell(this, cellIndex);
     checkWinner();
+
+    // Save the game state after each move
+    saveGameState();
 }
+
 function updateCell(cell, index){
     options[index] = currentPlayer;
     cell.textContent = currentPlayer;
 }
+
 function changePlayer(){
     currentPlayer = (currentPlayer == "X") ? "O" : "X";
     statusText.textContent = `${currentPlayer}'s turn`;
 }
+
 function checkWinner(){
     let roundWon = false;
 
@@ -62,19 +77,51 @@ function checkWinner(){
     if(roundWon){
         statusText.textContent = `${currentPlayer} wins!`;
         running = false;
+        // Save the game state when the game ends
+        saveGameState();
     }
     else if(!options.includes("")){
         statusText.textContent = `Draw!`;
         running = false;
+        // Save the game state when the game ends
+        saveGameState();
     }
     else{
         changePlayer();
     }
 }
+
 function restartGame(){
     currentPlayer = "X";
     options = ["", "", "", "", "", "", "", "", ""];
     statusText.textContent = `${currentPlayer}'s turn`;
     cells.forEach(cell => cell.textContent = "");
     running = true;
+    
+    // Save the game state when restarting the game
+    saveGameState();
+}
+
+function saveGameState() {
+    const gameState = {
+        options,
+        currentPlayer,
+        running
+    };
+
+    localStorage.setItem("ticTacToeGame", JSON.stringify(gameState));
+}
+
+function loadGameState() {
+    const savedGameState = localStorage.getItem("ticTacToeGame");
+    if (savedGameState) {
+        const gameState = JSON.parse(savedGameState);
+        options = gameState.options;
+        currentPlayer = gameState.currentPlayer;
+        running = gameState.running;
+
+        cells.forEach((cell, index) => {
+            cell.textContent = options[index];
+        });
+    }
 }
